b3f7ad85a7d2d8028f1cedcd9029e2710526ea20
Davi Ribeiro André <Daviribeiroandre67@gmail.com>
2023-02-19 09:06:28 -0300

    if or else

---
+++ statement/src/statement/Main.java
@@ -0,0 +1,20 @@
+package statement;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int age = 16;
+		
+		if(age>=75) {
+			System.out.println("boomer");//the order is very important
+		}
+		else if(age>=18) {
+			System.out.println("adult");
+		}
+		else {
+			System.out.println("kido");
+		}
+
+	}
+
+}
