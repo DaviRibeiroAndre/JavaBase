1a56cfbe78c835e166d51976a6306962a6218ecc
Davi Ribeiro André <Daviribeiroandre67@gmail.com>
2023-02-18 21:29:03 -0300

    Create Main.java

---
+++ vars/src/vars/Main.java
@@ -0,0 +1,30 @@
+package vars;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int x = 123;
+		double y = 3.14f;
+		boolean z = true;
+		char symbol ='@';
+		System.out.println(y+x);
+		System.out.println(z);
+		System.out.println(symbol);
+		String v ="wow";
+		String t ="uou"; 
+		String q;
+		//for change variables
+		
+		System.out.println(v);
+		System.out.println(t);
+		q=v;
+		v=t;
+		t=q;
+		System.out.println(v);
+		System.out.println(t);
+		
+		
+
+	}
+
+}
