cf60b895bee5e3afd05bc3d7765ec9d96e09cde7
Davi Ribeiro André <Daviribeiroandre67@gmail.com>
2023-02-19 09:45:10 -0300

    for

---
+++ forloop/src/forloop/Main.java
@@ -0,0 +1,14 @@
+package forloop;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		for (int i = 100;i>=0;i--) {//to be 2 in 2, i-=2 or i+=2
+			System.out.println(i);
+		}
+		System.out.println("boom");
+
+	}
+
+}
