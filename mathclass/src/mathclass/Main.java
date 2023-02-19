f02cea824a240e76ebc516d112c0a122813680e6
Davi Ribeiro André <Daviribeiroandre67@gmail.com>
2023-02-19 08:35:40 -0300

    math is cool

---
+++ mathclass/src/mathclass/Main.java
@@ -0,0 +1,17 @@
+package mathclass;
+
+public class Main {
+
+	public static void main(String[] args) {
+		double y = 9;
+		double x = 3.14f;
+		double z = Math.max(y, x);
+		
+		System.out.println(z);
+		
+		 double q = Math.sqrt(y);
+		System.out.println(q);
+
+	}
+
+}
