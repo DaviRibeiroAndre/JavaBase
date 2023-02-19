aa98b6caf8486ca4fa6c583e1ede3c529252b873
Davi Ribeiro André <Daviribeiroandre67@gmail.com>
2023-02-18 21:47:04 -0300

    Update Main.java

--- userinput/src/userinput/Main.java
+++ userinput/src/userinput/Main.java
@@ -9,11 +9,20 @@ public class Main {
 		Scanner scanner = new Scanner(System.in);
 		
 		System.out.println("what's your name?");
-		String name = scanner.nextLine(); // the data type is very importan
+		String name = scanner.nextLine(); // the data type is very important
 		System.out.println("hi "+name);
 		System.out.println("how old are you?");
 		int age = scanner.nextInt();
-		System.out.println("so, you have "+age);
+		System.out.println("so, you have "+age);// for more than one nextline, is needed put a scanner between
+		/* System.out.println("what's your name?");
+		String name = scanner.nextLine();
+		scanner.nextLine();
+		System.out.println("what's your second name?");
+		
+		
+		 * 
+		 * 
+		 */
 		
 	}
 
