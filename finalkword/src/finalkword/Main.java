8cbc8f11a29a5cd3e092462a364911ef7fa484ba
Davi Ribeiro André <Daviribeiroandre67@gmail.com>
2023-02-21 15:08:13 -0300

    finalkword

---
+++ finalkword/src/finalkword/Main.java
@@ -0,0 +1,16 @@
+package finalkword;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//a var impossible to change 
+		final double PI = 3.14;
+		
+		
+		//PI = 4;
+		
+		System.out.println(PI);
+
+	}
+
+}
